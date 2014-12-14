a126d8d7b5b9e9f137f158149cb415aefa954358
Nathen Drees <ndrees@sonomapartners.com>
2014-12-14 17:08:26 -0600

    Completed FIB challenge

---
+++ FIB/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let fibSeq k startNum =
+    let rec fibSeqHelper n0 n1 =
+        seq {
+            let n2 = (k * n0) + n1
+            yield n2
+            yield! fibSeqHelper n1 n2
+        }
+    let longStartNum = int64(startNum)
+    seq { yield longStartNum; yield longStartNum; yield! fibSeqHelper longStartNum longStartNum }
+
+[<EntryPoint>]
+let main argv = 
+    let n = Int32.Parse(argv.[0])
+    let k = Int64.Parse(argv.[1])
+    let count = Seq.nth (n - 1) (fibSeq k 1)
+    printfn "%i" count
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
