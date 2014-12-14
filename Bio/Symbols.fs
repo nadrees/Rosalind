8b7fb6731d97141e791762074db1e441026ad596
Nathen Drees <ndrees@sonomapartners.com>
2014-12-14 15:52:01 -0600

    Completed PROT challenge

--- Bio/Symbols.fs
+++ Bio/Symbols.fs
@@ -3,6 +3,7 @@
 module Symbols =
     type DNA = A | G | T | C
     type RNA = A | C | G | U
+    type AminoAcid = A | C | D | E | F | G | H | I | K | L | M | N | P | Q | R | S | T | U | V | W | Y | Stop
 
     let DNAtoRNA dnaSymbol = 
         match dnaSymbol with
@@ -10,6 +11,51 @@ module Symbols =
         | DNA.C -> RNA.C
         | DNA.G -> RNA.G
         | DNA.T -> RNA.U
+
+    let RNAtoAminoAcid rnaCodon =
+        match rnaCodon with
+        | (RNA.G, RNA.C, RNA.U) | (RNA.G, RNA.C, RNA.C) | (RNA.G, RNA.C, RNA.A) | (RNA.G, RNA.C, RNA.G) -> AminoAcid.A
+        | (RNA.U, RNA.G, RNA.U) | (RNA.U, RNA.G, RNA.C) -> AminoAcid.C
+        | (RNA.G, RNA.A, RNA.U) | (RNA.G, RNA.A, RNA.C) -> AminoAcid.D
+        | (RNA.G, RNA.A, RNA.A) | (RNA.G, RNA.A, RNA.G) -> AminoAcid.E
+        | (RNA.U, RNA.U, RNA.U) | (RNA.U, RNA.U, RNA.C) -> AminoAcid.F
+        | (RNA.G, RNA.G, RNA.U) | (RNA.G, RNA.G, RNA.C) | (RNA.G, RNA.G, RNA.A) | (RNA.G, RNA.G, RNA.G) -> AminoAcid.G
+        | (RNA.C, RNA.A, RNA.U) | (RNA.C, RNA.A, RNA.C) -> AminoAcid.H
+        | (RNA.A, RNA.U, RNA.U) | (RNA.A, RNA.U, RNA.C) | (RNA.A, RNA.U, RNA.A) -> AminoAcid.I
+        | (RNA.A, RNA.A, RNA.A) | (RNA.A, RNA.A, RNA.G) -> AminoAcid.K
+        | (RNA.U, RNA.U, RNA.A) | (RNA.U, RNA.U, RNA.G) | (RNA.C, RNA.U, RNA.U) | (RNA.C, RNA.U, RNA.C) | (RNA.C, RNA.U, RNA.A) | (RNA.C, RNA.U, RNA.G) -> AminoAcid.L
+        | (RNA.A, RNA.U, RNA.G) -> AminoAcid.M
+        | (RNA.A, RNA.A, RNA.U) | (RNA.A, RNA.A, RNA.C) -> AminoAcid.N
+        | (RNA.C, RNA.C, RNA.U) | (RNA.C, RNA.C, RNA.C) | (RNA.C, RNA.C, RNA.A) | (RNA.C, RNA.C, RNA.G) -> AminoAcid.P
+        | (RNA.C, RNA.A, RNA.A) | (RNA.C, RNA.A, RNA.G) -> AminoAcid.Q
+        | (RNA.C, RNA.G, RNA.U) | (RNA.C, RNA.G, RNA.C) | (RNA.C, RNA.G, RNA.A) | (RNA.C, RNA.G, RNA.G) | (RNA.A, RNA.G, RNA.A) | (RNA.A, RNA.G, RNA.G) -> AminoAcid.R
+        | (RNA.U, RNA.C, RNA.U) | (RNA.U, RNA.C, RNA.C) | (RNA.U, RNA.C, RNA.A) | (RNA.U, RNA.C, RNA.G) | (RNA.A, RNA.G, RNA.U) | (RNA.A, RNA.G, RNA.C) -> AminoAcid.S
+        | (RNA.A, RNA.C, RNA.U) | (RNA.A, RNA.C, RNA.C) | (RNA.A, RNA.C, RNA.A) | (RNA.A, RNA.C, RNA.G) -> AminoAcid.T
+        | (RNA.G, RNA.U, RNA.U) | (RNA.G, RNA.U, RNA.C) | (RNA.G, RNA.U, RNA.A) | (RNA.G, RNA.U, RNA.G) -> AminoAcid.V
+        | (RNA.U, RNA.G, RNA.G) -> AminoAcid.W
+        | (RNA.U, RNA.A, RNA.U) | (RNA.U, RNA.A, RNA.C) -> AminoAcid.Y
+        | (RNA.U, RNA.A, RNA.A) | (RNA.U, RNA.A, RNA.G) | (RNA.U, RNA.G, RNA.A) -> AminoAcid.Stop
+
+    let RNASeqToRNACodons (rnaSeq : seq<RNA>) : seq<(RNA * RNA * RNA)> =
+        let tupleToCodon (tuple : (int * seq<int * RNA>)) =
+            let unpackRNA rnaTuple = 
+                match rnaTuple with
+                | (_, rna) -> rna
+
+            let groupedRNA = snd (tuple)
+            let firstRNA = Seq.nth 0 groupedRNA
+            let secondRNA = Seq.nth 1 groupedRNA
+            let thirdRNA = Seq.nth 2 groupedRNA
+            (unpackRNA firstRNA, unpackRNA secondRNA, unpackRNA thirdRNA)
+
+        let seqLength = (Seq.toList rnaSeq).Length
+        if seqLength % 3 <> 0 then
+            raise (new System.ArgumentException(sprintf "RNA Sequence length must be a multiple of 3. Length was %i" seqLength))
+        else
+            let indexedRNA = Seq.mapi (fun index rna -> (index, rna)) rnaSeq
+            let groupedRNA = Seq.groupBy (fun tuple -> (fst (tuple)) / 3) indexedRNA
+            let rnaCodons = Seq.map tupleToCodon groupedRNA
+            rnaCodons
 
     let TryParseDNACharacter c =
         match c with
@@ -29,6 +75,11 @@ module Symbols =
 
     let ParseDNACharacter c =
         match TryParseDNACharacter c with
+        | (true, Some s) -> s
+        | _ -> raise (new System.ArgumentException("Invalid character " + c.ToString()))
+
+    let ParseRNACharacter c = 
+        match TryParseRNACharacter c with
         | (true, Some s) -> s
         | _ -> raise (new System.ArgumentException("Invalid character " + c.ToString()))
 
---
+++ PROT/Program.fs
@@ -0,0 +1,8 @@
+﻿open Bio.Symbols
+
+[<EntryPoint>]
+let main argv = 
+    let fileLine = System.IO.File.ReadAllText("input.txt")
+    let aminoString = Seq.map ParseRNACharacter fileLine |> RNASeqToRNACodons |> Seq.map RNAtoAminoAcid |> Seq.takeWhile (fun s -> s <> AminoAcid.Stop)
+    System.IO.File.WriteAllText("output.txt", (PrintString aminoString))
+    0 // return an integer exit code
