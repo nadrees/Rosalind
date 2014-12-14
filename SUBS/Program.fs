1ebce4b84757f04616a4a8ea69070e4d3f0475bc
Nathen Drees <ndrees@sonomapartners.com>
2014-12-14 12:59:31 -0600

    Completed SUBS challenge

---
+++ SUBS/Program.fs
@@ -0,0 +1,25 @@
+﻿open Utilities.IO
+
+let findSubstrPositions (str1 : string) (str2 : string) =
+    let rec findSubstrPositionsHelper currentPositions = 
+        match currentPositions with
+        | [] ->
+            match str1.IndexOf(str2) with
+            | -1 -> currentPositions
+            | x -> findSubstrPositionsHelper (x::currentPositions)
+        | _ ->
+            let startIndex = (List.head currentPositions) + 1
+            if (startIndex = str1.Length) then
+                currentPositions
+            else
+                match str1.IndexOf(str2, startIndex) with
+                | -1 -> currentPositions
+                | x -> findSubstrPositionsHelper (x::currentPositions)
+    List.rev (findSubstrPositionsHelper List.Empty)
+
+[<EntryPoint>]
+let main argv = 
+    let fileLines = System.IO.File.ReadAllLines("input.txt")
+    let substrPositions = List.map (fun x -> x + 1) (findSubstrPositions (Seq.nth 0 fileLines) (Seq.nth 1 fileLines))
+    writeLineToFile "output.txt" (List.toArray substrPositions)
+    0 // return an integer exit code
