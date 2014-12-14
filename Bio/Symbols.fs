4c10ad6483a0553397c513affa554a73a28f92fa
Nathen Drees <ndrees@sonomapartners.com>
2014-12-13 23:36:37 -0600

    Completed RNA challenge

---
+++ Bio/Symbols.fs
@@ -0,0 +1,28 @@
+﻿namespace Bio
+
+module Symbols =
+    type DNA = A | G | T | C
+    type RNA = A | C | G | U
+
+    let DNAtoRNA dnaSymbol = 
+        match dnaSymbol with
+        | DNA.A -> RNA.A
+        | DNA.C -> RNA.C
+        | DNA.G -> RNA.G
+        | DNA.T -> RNA.U
+
+    let TryParseDNACharacter c =
+        match c with
+        | 'A' -> (true, Some(DNA.A))
+        | 'C' -> (true, Some(DNA.C))
+        | 'G' -> (true, Some(DNA.G))
+        | 'T' -> (true, Some(DNA.T))
+        | _ -> (false, None)
+
+    let TryParseRNACharacter c =
+        match c with
+        | 'A' -> (true, Some(RNA.A))
+        | 'C' -> (true, Some(RNA.C))
+        | 'G' -> (true, Some(RNA.G))
+        | 'U' -> (true, Some(RNA.U))
+        | _ -> (false, None)
---
+++ RNA/Program.fs
@@ -0,0 +1,16 @@
+﻿open Bio.Symbols
+
+let parseCharacter c =
+    match TryParseDNACharacter c with
+    | (true, Some s) -> s
+    | _ -> raise (new System.ArgumentException("Invalid character " + c.ToString()))
+
+[<EntryPoint>]
+let main argv = 
+    let inputString = argv.[0]
+    let dnaCharacters = Seq.map parseCharacter inputString
+    let rnaCharacters = Seq.map DNAtoRNA dnaCharacters
+    let str = Seq.fold (fun s rna -> sprintf "%s%A" s rna) "" rnaCharacters
+    printfn "%A" str
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
