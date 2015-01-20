c6575b4c921fcc68057543a9fae3ad5e2e6e36ae
Nathen Drees <nadrees@gmail.com>
2015-01-19 22:00:10 -0600

    Completed FIBD challenge

---
+++ FIBD/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.Numerics
+
+let calcMortalFib numGenerations lifeSpan =
+    let rec calcNextGeneration populationStack generationNumber = 
+        match generationNumber with
+        | x when x = numGenerations -> List.sum populationStack
+        | _ ->
+            let offspringThisGeneration = List.tail populationStack |> List.sum // only members alive for more than 1 generation can breed
+            let nextGenerationStack = Seq.truncate lifeSpan (offspringThisGeneration :: populationStack) |> Seq.toList
+            calcNextGeneration nextGenerationStack (generationNumber + 1)
+    calcNextGeneration [new BigInteger(1)] 1
+
+[<EntryPoint>]
+let main argv = 
+    let n = Int32.Parse argv.[0]
+    let m = Int32.Parse argv.[1]
+    let answer = calcMortalFib n m
+    printfn "%A" answer
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
