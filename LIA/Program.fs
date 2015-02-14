4687faea78aadb6436c99b87cda425e40b734848
Nathen Drees <nadrees@gmail.com>
2015-02-13 21:32:36 -0600

    Solved LIA challenge

---
+++ LIA/Program.fs
@@ -0,0 +1,15 @@
+﻿open MathNet.Numerics
+
+let binomialExperiment n k =
+    let p = 0.25
+    SpecialFunctions.Binomial(k, n) * (pown p n) * (pown 0.75 (k - n))
+
+[<EntryPoint>]
+let main argv = 
+    let k = int(argv.[0])
+    let N = int(argv.[1])
+    let probabilities = Seq.init N (fun n -> binomialExperiment n (pown 2 k))
+    let answer = 1.0 - Seq.sum probabilities
+    printfn "%f" answer
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
